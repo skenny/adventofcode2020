b1ae5e37fa36a640f660465d18d8361d18c9b083
Shaun Kenny <skenny@gmail.com>
2020-12-03 15:50:23 -0400

    day 3

---
+++ day3.py
@@ -0,0 +1,25 @@
+input = []
+with open('day3-input', 'r') as input_file:
+    input = [v.strip() for v in input_file.readlines()]
+
+def traverse(h, v):
+    x = 0
+    y = 0
+    tree_count = 0
+    while y < len(input):
+        row = input[y]
+        c = row[x % len(row)]
+        if c == "#":
+            tree_count += 1
+        x += h
+        y += v
+    return tree_count
+
+def part1():
+    print('part 1: encountered', traverse(3, 1), 'trees')
+
+def part2():
+    print('part 2: encountered', traverse(1, 1) * traverse(3, 1) * traverse(5, 1) * traverse(7, 1) * traverse(1, 2), 'trees')
+
+part1()
+part2()
