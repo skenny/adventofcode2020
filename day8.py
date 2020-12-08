ed4afc221538bdb6b5a889d1d8b785b907fe94fc
Shaun Kenny <skenny@gmail.com>
2020-12-08 19:19:11 -0400

    refactoring

--- day8.py
+++ day8.py
@@ -1,7 +1,8 @@
 INPUT_FILE = "day8-input"
+TEST_INPUT_FILE = "day8-input-test"
 
-def read_input():
-    with open(INPUT_FILE, "r") as fin:
+def read_input(file):
+    with open(file, "r") as fin:
         return [l.strip() for l in fin.readlines()]
 
 def parse_boot_code(input):
@@ -14,14 +15,14 @@ def parse_boot_code(input):
 def execute(boot_code):
     accumulator = 0
     instr_ptr = 0
-    loop = False
+    success = True
 
     for instr in boot_code:
         instr["executed"] = False
 
     while True:
         if instr_ptr < 0 or instr_ptr >= len(boot_code):
-            print("exit: instr_ptr is out of range: {} (0..{})".format(instr_ptr, len(boot_code) - 1))
+            #print("exit: instr_ptr is out of range: {} (0..{})".format(instr_ptr, len(boot_code) - 1))
             break
 
         instr = boot_code[instr_ptr]
@@ -31,55 +32,36 @@ def execute(boot_code):
 
         if executed:
             #print("already executed instruction {}; aborting...".format(instr_ptr))
-            loop = True
+            success = False
             break
-        
-        instr["executed"] = True
 
+        jump_ptr = 1
         if (op == "acc"):
             accumulator += arg
-            instr_ptr += 1
         elif (op == "jmp"):
-            instr_ptr += arg
+            jump_ptr = arg
         elif (op == "nop"):
-            instr_ptr += 1
+            pass
+        
+        instr["executed"] = True
+        instr_ptr += jump_ptr
 
-    return (accumulator, loop)
+    return (accumulator, success)
 
 def repair(boot_code):
     for instr in boot_code:
-        if (instr["op"] == "nop"):
-            instr["op"] = "jmp"
+        if instr["op"] in ("nop", "jmp"):
+            swapped = instr["op"]
+            instr["op"] = "jmp" if swapped == "nop" else "nop"
             result = execute(boot_code)
-            if not result[1]:
+            if result[1]:
                 return result
-            instr["op"] = "nop"
-        if (instr["op"] == "jmp"):
-            instr["op"] = "nop"
-            result = execute(boot_code)
-            if not result[1]:
-                return result
-            instr["op"] = "jmp"
+            instr["op"] = swapped
 
-def run_tests():
-    test_boot_code = parse_boot_code([
-        "nop +0",
-        "acc +1",
-        "jmp +4",
-        "acc +3",
-        "jmp -3",
-        "acc -99",
-        "acc +1",
-        "jmp -4",
-        "acc +6",
-    ])
-    print("test 1", execute(test_boot_code))
-    print("test 2", repair(test_boot_code))
+def run(label, input_file):
+    boot_code = parse_boot_code(read_input(input_file))
+    print("{} 1: {}".format(label, execute(boot_code)))
+    print("{} 2: {}".format(label, repair(boot_code)))
 
-def run_parts():
-    boot_code = parse_boot_code(read_input())
-    print("part 1", execute(boot_code))
-    print("part 2", repair(boot_code))
-
-run_tests()
-run_parts()
+run("test", TEST_INPUT_FILE)
+run("part", INPUT_FILE)
