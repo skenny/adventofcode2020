b49395f1e32c49e59d599a0146c194019e764297
Shaun Kenny <skenny@gmail.com>
2020-12-10 22:35:04 -0400

    cleanup

--- day10.py
+++ day10.py
@@ -3,20 +3,23 @@ TEST_INPUT_FILE_1 = "day10-input-test"
 TEST_INPUT_FILE_2 = "day10-input-test-2"
 
 DEVICE_ADAPTER_JOLTAGE_ADJ = 3
+MAX_JOLTAGE_DIFFERENCE = 3
+OUTLET_JOLTAGE = 0
 
 def read_input(file):
     with open(file, "r") as fin:
         return [int(l) for l in fin.readlines()]
 
-def plug_in(adapters):
-    joltage = 0
+def connect(adapters):
+    joltage = OUTLET_JOLTAGE
     deltas = []
     while len(adapters) > 0:
-        adapter = min(list(filter(lambda adapter_joltage: adapter_joltage - joltage <= 3, adapters)))        
+        adapter = min(list(filter(lambda adapter_joltage: adapter_joltage - joltage <= MAX_JOLTAGE_DIFFERENCE, adapters)))
         adapters.remove(adapter)
         deltas.append(adapter - joltage)
         joltage = adapter
 
+    # account for built in adapter joltage
     joltage += DEVICE_ADAPTER_JOLTAGE_ADJ
     deltas.append(DEVICE_ADAPTER_JOLTAGE_ADJ)
 
@@ -24,7 +27,7 @@ def plug_in(adapters):
 
 def run(label, file):
     adapters = read_input(file)
-    print("{} 1: {}".format(label, plug_in(adapters)))
+    print("{} 1: {}".format(label, connect(adapters)))
 
 run("test 1", TEST_INPUT_FILE_1)
 run("test 2", TEST_INPUT_FILE_2)
