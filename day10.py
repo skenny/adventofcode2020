fa9d728f36cde59970f4437e677ac4772ea2b939
Shaun Kenny <skenny@gmail.com>
2020-12-11 14:59:16 -0400

    day 10 cleanup

--- day10.py
+++ day10.py
@@ -13,73 +13,51 @@ def read_input(file):
 
 def connect(adapters):
     working_adapters = adapters.copy()
-    joltage = OUTLET_JOLTAGE
-    deltas = []
+    joltage = adapters[0]
 
-    while len(working_adapters) > 0:
-        qualifying_adapters = set(filter(lambda adapter_joltage: adapter_joltage - joltage <= MAX_JOLTAGE_DIFFERENCE, working_adapters))
+    # we always connect the built-in adapter to the last adapter
+    deltas = [DEVICE_ADAPTER_JOLTAGE_ADJ]
 
-        # always take the lowest joltage adapter, to ensure all adapters are used
-        adapter = min(qualifying_adapters)
-
+    for adapter in adapters:
         working_adapters.remove(adapter)
-        deltas.append(adapter - joltage)
-        joltage = adapter
-    
-    # account for built in adapter joltage
-    joltage += DEVICE_ADAPTER_JOLTAGE_ADJ
-    deltas.append(DEVICE_ADAPTER_JOLTAGE_ADJ)
+        next_adapter_options = set(filter(lambda adapter_joltage: adapter_joltage - joltage <= MAX_JOLTAGE_DIFFERENCE, working_adapters))
+
+        if not next_adapter_options:
+            break
+
+        # always connect to the lowest joltage adapter option to ensure all adapters are used
+        next_adapter = min(next_adapter_options)
+
+        deltas.append(next_adapter - joltage)
+        joltage = next_adapter
 
     return deltas.count(1) *  deltas.count(3)
 
-def build_graph(all_adapters):
+def count_paths(adapters):
     graph = {}
-
-    adapters = all_adapters.copy()
-    adapters.insert(0, 0)
-
     for i, adapter in enumerate(adapters):
         remaining_adapters = adapters[i + 1:]
-        if not graph.get(adapter):
-            graph[adapter] = set(filter(lambda other_adapter: other_adapter - adapter <= MAX_JOLTAGE_DIFFERENCE, remaining_adapters))
-
-    #print(adapters, graph)
-    return graph
-
-def count_paths(adapters):
-    graph = build_graph(adapters)
-    path_counts = {}
+        graph[adapter] = set(filter(lambda other_adapter: other_adapter - adapter <= MAX_JOLTAGE_DIFFERENCE, remaining_adapters))
     
-    graph_adapters = list(graph.keys())
-    graph_adapters.sort()
-
-    start = min(graph_adapters)
-
-    for adapter in reversed(graph_adapters):
+    path_counts = {}
+    for adapter in reversed(adapters):
         adapter_options = graph[adapter]
         if not adapter_options:
             path_counts[adapter] = 1
         else:
             path_counts[adapter] = sum([path_counts[i] for i in adapter_options])
     
-    return path_counts[start]
+    return path_counts[min(adapters)]
 
 def run(label, file):
     adapters = read_input(file)
     adapters.sort()
+    adapters.insert(0, OUTLET_JOLTAGE)
+
     print("{} 1: {}".format(label, connect(adapters)))
     print("{} 2: {}".format(label, count_paths(adapters)))
 
-run("test 1", TEST_INPUT_FILE_1)
-run("test 2", TEST_INPUT_FILE_2)
-run("test 3", TEST_INPUT_FILE_3)
+run("test_1", TEST_INPUT_FILE_1)
+run("test_2", TEST_INPUT_FILE_2)
+run("test_3", TEST_INPUT_FILE_3)
 run("part", INPUT_FILE)
-
-'''
-0 1 2 3 6
----------
-0 1 2 3 6
-0 1 3 6
-0 2 3 6
-0 3 6
-'''
