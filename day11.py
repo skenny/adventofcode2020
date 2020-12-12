8fdb6da1f3f4ddd9052660c3e986d81c94ad1530
Shaun Kenny <skenny@gmail.com>
2020-12-11 23:37:00 -0400

    setup for pt2

--- day11.py
+++ day11.py
@@ -1,3 +1,5 @@
+import math
+
 INPUT_FILE = "day11-input"
 TEST_INPUT_FILE = "day11-input-test"
 
@@ -8,7 +10,7 @@ def read_input(file):
     with open(file, "r") as fin:
         return [list(l.strip()) for l in fin.readlines()]
 
-def fill_seats_1(grid):
+def fill_seats(grid, seat_counter):
     #print_grid(grid)
 
     new_grid = []
@@ -17,7 +19,7 @@ def fill_seats_1(grid):
     for i, row in enumerate(grid):
         new_row = []
         for j, col in enumerate(row):
-            occupied_adjacent = count_occupied_adjacent_seats(grid, i, j)
+            occupied_adjacent = seat_counter(grid, i, j) #count_occupied_adjacent_seats(grid, i, j)
             if col == EMPTY and occupied_adjacent == 0:
                 new_row.append(OCCUPIED)
                 num_seats_changed += 1
@@ -30,6 +32,9 @@ def fill_seats_1(grid):
 
     #print_grid(new_grid)
     return (new_grid, num_seats_changed)
+
+def find_angle_between(x, y, x2, y2):
+    return (360 - ((math.atan2(y - y2, x2 - x) * 180 / math.pi) - 90)) % 360
 
 def count_occupied_seats(grid):
     return sum(list(map(lambda row: row.count(OCCUPIED), grid)))
@@ -49,14 +54,17 @@ def count_occupied_adjacent_seats(grid, row, col):
     
     return adjacent_seats.count(OCCUPIED)
 
+def count_occupied_in_sight_seats(grid, row, col):
+    return 0
+
 def print_grid(grid):
     [print("".join(r)) for r in grid]
     print("-" * len(grid[0]))
 
-def part1(grid):
+def adjust_seating(grid, seat_counter):
     working_grid = [row.copy() for row in grid]
     while True:
-        new_grid, num_seats_changed = fill_seats_1(working_grid)
+        new_grid, num_seats_changed = fill_seats(working_grid, seat_counter)
         if num_seats_changed == 0:
             break
         working_grid = new_grid
@@ -64,7 +72,8 @@ def part1(grid):
 
 def run(label, input_file):
     grid = read_input(input_file)
-    print("{} 1: {}".format(label, part1(grid)))
+    print("{} 1: {}".format(label, adjust_seating(grid, count_occupied_adjacent_seats)))
+    print("{} 2: {}".format(label, adjust_seating(grid, count_occupied_in_sight_seats)))
 
 run("test", TEST_INPUT_FILE)
 #run("part", INPUT_FILE)
