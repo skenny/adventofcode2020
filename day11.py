5be0167e7ccd687f85a9b4d19c9b944bf2ef4b51
Shaun Kenny <skenny@gmail.com>
2020-12-12 14:41:47 -0400

    refactoring

--- day11.py
+++ day11.py
@@ -11,29 +11,46 @@ def read_input(file):
         return [list(l.strip()) for l in fin.readlines()]
 
 def fill_seats(grid, occupied_seat_counter, occupied_threshold):
-    #print("fill_seats using", occupied_seat_counter, occupied_threshold)
-    #print_grid(grid)
+    #print("fill seats using", occupied_seat_counter, occupied_threshold)
 
-    new_grid = []
-    num_seats_changed = 0
+    working_grid = [row.copy() for row in grid]
+    iteration = 1
 
-    for i, row in enumerate(grid):
-        new_row = []
-        for j, col in enumerate(row):
-            occupied_seats = occupied_seat_counter(grid, i, j) if not col == "." else 0
-            if col == EMPTY and occupied_seats == 0:
-                new_row.append(OCCUPIED)
-                num_seats_changed += 1
-            elif col == OCCUPIED and occupied_seats >= occupied_threshold:
-                new_row.append(EMPTY)
-                num_seats_changed += 1
-            else:
-                new_row.append(col)
-        new_grid.append(new_row)
+    while True:
+        new_grid = []
+        num_seats_changed = 0
 
-    return (new_grid, num_seats_changed)
+        #print_grid(working_grid)
 
-def count_occupied_seats(grid):
+        for i, row in enumerate(working_grid):
+            new_row = []
+
+            for j, col in enumerate(row):
+                occupied_seats = 0
+                if not col == ".":
+                    occupied_seats = occupied_seat_counter(working_grid, i, j)
+                if col == EMPTY and occupied_seats == 0:
+                    new_row.append(OCCUPIED)
+                    num_seats_changed += 1
+                elif col == OCCUPIED and occupied_seats >= occupied_threshold:
+                    new_row.append(EMPTY)
+                    num_seats_changed += 1
+                else:
+                    new_row.append(col)
+
+            new_grid.append(new_row)
+
+        print(iteration, num_seats_changed, count_occupied_grid_seats(working_grid))
+
+        if num_seats_changed == 0:
+            break
+        
+        working_grid = new_grid
+        iteration += 1
+
+    return count_occupied_grid_seats(working_grid)
+
+def count_occupied_grid_seats(grid):
     return sum(list(map(lambda row: row.count(OCCUPIED), grid)))
 
 def count_occupied_adjacent_seats(grid, row, col):
@@ -81,19 +98,10 @@ def print_grid(grid):
     [print("".join(r)) for r in grid]
     print("-" * len(grid[0]))
 
-def adjust_seating(grid, seat_counter, occupied_threshold):
-    working_grid = [row.copy() for row in grid]
-    while True:
-        new_grid, num_seats_changed = fill_seats(working_grid, seat_counter, occupied_threshold)
-        if num_seats_changed == 0:
-            break
-        working_grid = new_grid
-    return count_occupied_seats(working_grid)
-
 def run(label, input_file):
     grid = read_input(input_file)
-    print("{} 1: {}".format(label, adjust_seating(grid, count_occupied_adjacent_seats, 4)))
-    print("{} 2: {}".format(label, adjust_seating(grid, count_occupied_in_sight_seats, 5)))
+    print("{} 1: {}".format(label, fill_seats(grid, count_occupied_adjacent_seats, 4)))
+    print("{} 2: {}".format(label, fill_seats(grid, count_occupied_in_sight_seats, 5)))
 
 run("test", TEST_INPUT_FILE)
 run("part", INPUT_FILE)
