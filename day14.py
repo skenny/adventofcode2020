57b31d00e17d0aff3cc8c34bf5f89d8e953a9919
Shaun Kenny <skenny@gmail.com>
2020-12-14 13:20:42 -0400

    day 14 pt 1 refactoring

--- day14.py
+++ day14.py
@@ -13,30 +13,30 @@ def int_to_bit_field(i):
 def bit_field_to_int(bits):
     return int("".join(str(i) for i in bits), 2)
 
+def apply_mask(value, bit_mask):
+    bit_value = list(reversed(int_to_bit_field(value)))
+    masked_bit_value = []
+
+    for i, mask_bit in enumerate(reversed(bit_mask)):
+        new_bit_value = bit_value[i] if i < len(bit_value) else 0
+        if mask_bit != "X":
+            new_bit_value = int(mask_bit)
+        masked_bit_value.insert(0, new_bit_value)
+
+    return bit_field_to_int(masked_bit_value)
+
 def part1(program):
     bit_mask = ['0'] * 36
     mem = {}
     for line in program:
         if line.startswith("mask"):
             bit_mask = list(line.split(" = ")[1])
-            bit_mask.reverse()
-            #print("using mask", bit_mask)
         if line.startswith("mem"):
             search_result = re.search(r"mem\[(\d+)\] = (\d+)", line)
             if search_result:
                 address = search_result.group(1)
                 value = int(search_result.group(2))
-                bit_value = int_to_bit_field(value)
-                bit_value.reverse()
-                masked_bit_value = []
-                for i, mask_bit in enumerate(bit_mask):
-                    v = bit_value[i] if i < len(bit_value) else 0
-                    new_bit_value = v
-                    if mask_bit != "X":
-                        new_bit_value = int(mask_bit)
-                    masked_bit_value.insert(0, new_bit_value)
-                #print("masked", bit_value, "->", masked_bit_value)
-                mem[address] = bit_field_to_int(masked_bit_value)
+                mem[address] = apply_mask(value, bit_mask)
     return sum(mem.values())
 
 def run(label, input_file):
