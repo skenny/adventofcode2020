cfd44a088da27a79a53a196a199d8daed4822445
Shaun Kenny <skenny@gmail.com>
2020-12-16 14:04:59 -0400

    day 16 part 2

--- day16.py
+++ day16.py
@@ -27,7 +27,7 @@ def read_input(file):
                 rule_ranges = []
                 for rule_range in rule_spec.split(" or "):
                     low, high = rule_range.split("-")
-                    rule_ranges.append(range(int(low), int(high) + 1))
+                    rule_ranges.append(range(int(low), int(high)))
                 rules[rule_name] = rule_ranges
             if mode == 1:
                 your_ticket = [int(v) for v in l.split(",")]
@@ -41,34 +41,67 @@ def validate_tickets(rules, tickets):
     valid_tickets = []
 
     for ticket in tickets:
+        ticket_valid = True
         for n in ticket:
-            valid = False
+            num_valid = False
             for rule_ranges in rules.values():
-                for rule_range in rule_ranges:
-                    if n in rule_range:
-                        valid = True
-                        break
-                if valid:
+                if check_rule(n, rule_ranges):
+                    num_valid = True
                     break
-
-            if valid:
-                valid_tickets.append(ticket)
-            else:
+            if not num_valid:
+                ticket_valid = False
                 invalid_count += n
+        if ticket_valid:
+            valid_tickets.append(ticket)
 
     return (invalid_count, valid_tickets)
 
+def check_rule(n, rule_ranges):
+    matches = False
+    for rule_range in rule_ranges:
+        if rule_range[0] <= n <= rule_range[-1] + 1:
+            matches = True
+    return matches
+
 def match_ticket_fields(rules, your_ticket, nearby_tickets):
-    return
+    rule_matches = {}
+
+    # determine which rules are valid for which indexes
+    for rule, rule_ranges in rules.items():
+        for i in range(len(your_ticket)):
+            ticket_values_i = list(map(lambda ticket: ticket[i], nearby_tickets))
+            matches_rule = True
+            for n in ticket_values_i:
+                if not check_rule(n, rule_ranges):
+                    matches_rule = False
+                    break
+            if matches_rule:
+                rule_matches.setdefault(i, []).append(rule)
+
+    # reduce index->rule matches until each index matches a single rule
+    while True:
+        multi_matches = list(filter(lambda tup: len(tup[1]) > 1, rule_matches.items()))
+        if len(multi_matches) == 0:
+            break
+        single_matches = list(filter(lambda tup: len(tup[1]) == 1, rule_matches.items()))
+        for index, matched_rules in multi_matches:
+            for single_index, single_matched_rules in single_matches:
+                r = single_matched_rules[0]
+                if r in matched_rules:
+                    matched_rules.remove(r)
+
+    product = 1
+    for index, rule in rule_matches.items():
+        if rule[0].startswith("departure"):
+            product *= your_ticket[index]
+    return product
 
 def run(label, input_file):
     rules, your_ticket, nearby_tickets = read_input(input_file)
-
     error_rate, valid_tickets = validate_tickets(rules, nearby_tickets)
-    aaa = match_ticket_fields(rules, your_ticket, valid_tickets)
 
     print("{} 1: {}".format(label, error_rate))
-    print("{} 2: {}".format(label, aaa))
+    print("{} 2: {}".format(label, match_ticket_fields(rules, your_ticket, valid_tickets)))
 
 run("test 1", TEST_INPUT_FILE)
 run("test 2", TEST_INPUT_FILE_2)
