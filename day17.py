3eec129ba960beb5f142cbd0d3965f37fb32f0f2
Shaun Kenny <skenny@gmail.com>
2020-12-17 22:29:45 -0400

    day 17 part 2

--- day17.py
+++ day17.py
@@ -14,38 +14,43 @@ class PocketDimension:
     max_y = 0
     min_z = 0
     max_z = 0
+    min_w = 0
+    max_w = 0
 
     def setup_grid(self, grid):
         for y, row in enumerate(grid):
             for x, col in enumerate(row):
-                self.update_cube(x, y, 0, col)
+                self.update_cube(x, y, 0, 0, col)
 
-    def get_cube(self, x, y, z):
-        return self.cubes.get((x, y, z), INACTIVE)
+    def get_cube(self, x, y, z, w):
+        return self.cubes.get((x, y, z, w), INACTIVE)
 
-    def activate_cube(self, x, y, z):
-        self.update_cube(x, y, z, ACTIVE)
+    def activate_cube(self, x, y, z, w):
+        self.update_cube(x, y, z, w, ACTIVE)
 
-    def deactivate_cube(self, x, y, z):
-        self.update_cube(x, y, z, INACTIVE)
+    def deactivate_cube(self, x, y, z, w):
+        self.update_cube(x, y, z, w, INACTIVE)
 
-    def update_cube(self, x, y, z, state):
-        self.cubes[(x, y, z)] = state
+    def update_cube(self, x, y, z, w, state):
+        self.cubes[(x, y, z, w)] = state
         if x > self.max_x: self.max_x = x
         if x < self.min_x: self.min_x = x
         if y > self.max_y: self.max_y = y
         if y < self.min_y: self.min_y = y
         if z > self.max_z: self.max_z = z
         if z < self.min_z: self.min_z = z
+        if w > self.max_w: self.max_w = w
+        if w < self.min_w: self.min_w = w
 
-    def count_active_neighbours(self, x, y, z):
+    def count_active_neighbours(self, x, y, z, w):
         offsets = (-1, 0, 1)
         neighbours = []
         for xo in offsets:
             for yo in offsets:
                 for zo in offsets:
-                    if not (xo == 0 and yo == 0 and zo == 0):
-                        neighbours.append(self.get_cube(x + xo, y + yo, z + zo))
+                    for wo in offsets:
+                        if not (xo == 0 and yo == 0 and zo == 0 and wo == 0):
+                            neighbours.append(self.get_cube(x + xo, y + yo, z + zo, w + wo))
         return neighbours.count(ACTIVE)
 
     def count_active_cubes(self):
@@ -63,17 +68,20 @@ class PocketDimension:
         copy_version.max_y = self.max_y
         copy_version.min_z = self.min_z
         copy_version.max_z = self.max_z
+        copy_version.min_w = self.min_w
+        copy_version.max_w = self.max_w
         return copy_version
 
     def print(self):
-        for z in range(self.min_z, self.max_z + 1):
-            grid = []
-            for y in range(self.min_y, self.max_y + 1):
-                row = ""
-                for x in range(self.min_x, self.max_x + 1):
-                    row += self.get_cube(x, y, z)
-                grid.append(row)
-            print("z={}:\n{}\n{}".format(z, "\n".join(grid), "=" * len(grid[0])))
+        for w in range(self.min_w, self.max_w + 1):
+            for z in range(self.min_z, self.max_z + 1):
+                grid = []
+                for y in range(self.min_y, self.max_y + 1):
+                    row = ""
+                    for x in range(self.min_x, self.max_x + 1):
+                        row += self.get_cube(x, y, z)
+                    grid.append(row)
+                print("z={}, w={}\n{}\n{}".format(z, w, "\n".join(grid), "=" * len(grid[0])))
 
 def read_input(file):
     with open(file, "r") as fin:
@@ -92,16 +100,17 @@ def run(label, input_file):
         for x in range(pocket_dimension.min_x - 1, pocket_dimension.max_x + 2):
             for y in range(pocket_dimension.min_y - 1, pocket_dimension.max_y + 2):
                 for z in range(pocket_dimension.min_z - 1, pocket_dimension.max_z + 2):
-                    cube = pocket_dimension.get_cube(x, y, z)
-                    num_active_neighbours = pocket_dimension.count_active_neighbours(x, y, z)
-                    if cube == ACTIVE and num_active_neighbours not in (2, 3):
-                        new_pocket_dimension.deactivate_cube(x, y, z)
-                    if cube == INACTIVE and num_active_neighbours == 3:
-                        new_pocket_dimension.activate_cube(x, y, z)
-                        
+                    for w in range(pocket_dimension.min_w - 1, pocket_dimension.max_w + 2):
+                        cube = pocket_dimension.get_cube(x, y, z, w)
+                        num_active_neighbours = pocket_dimension.count_active_neighbours(x, y, z, w)
+                        if cube == ACTIVE and num_active_neighbours not in (2, 3):
+                            new_pocket_dimension.deactivate_cube(x, y, z, w)
+                        if cube == INACTIVE and num_active_neighbours == 3:
+                            new_pocket_dimension.activate_cube(x, y, z, w)
+
         pocket_dimension = new_pocket_dimension
 
-    print("{} 1: {}".format(label, pocket_dimension.count_active_cubes()))
+    print("{}: {}".format(label, pocket_dimension.count_active_cubes()))
 
 run("test", TEST_INPUT_FILE)
-#run("part", INPUT_FILE)
+run("part", INPUT_FILE)
