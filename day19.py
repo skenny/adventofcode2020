f4fca81bd780d6d2346f176d329bbce0bebd8758
Shaun Kenny <skenny@gmail.com>
2020-12-20 15:17:34 -0400

    day 19 non working part 2

--- day19.py
+++ day19.py
@@ -1,7 +1,5 @@
 import re
-
-INPUT_FILE = "day19-input"
-TEST_INPUT_FILE = "day19-input-test"
+import copy
 
 def read_input(file):
     rules = {}
@@ -29,41 +27,51 @@ def read_input(file):
     return rules, messages
 
 def simplify(rules):
-    rules_to_simplify = list(rules.keys())
+    rules_copy = copy.copy(rules)
+    rules_to_simplify = list(rules_copy.keys())
 
     while len(rules_to_simplify) > 0:
         for rule_num in rules_to_simplify:
-            rule_spec = rules[rule_num]
+            rule_spec = rules_copy[rule_num]
             #print("{}: {}".format(rule_num, rule_spec))
             if not any(char.isdigit() for char in rule_spec):
                 #print("simple rule", rule_num, rule_spec)
                 rules_to_simplify.remove(rule_num)
             else:
                 new_rule_spec = ""
-                for i in rule_spec.split(" "):
-                    if i.isdigit() and not int(i) in rules_to_simplify:
-                        simple_rule = rules[int(i)]
+                rule_parts = rule_spec.split(" ")
+                for i, rule_part in enumerate(rule_parts):
+                    if rule_part.isdigit() and int(rule_part) == rule_num:
+                        new_rule_spec = new_rule_spec + "+ "
+                    elif rule_part.isdigit() and not int(rule_part) in rules_to_simplify:
+                        simple_rule = rules_copy[int(rule_part)]
                         if len(simple_rule) == 1:
                             new_rule_spec += simple_rule + " "
                         else:
                             new_rule_spec += "(" + simple_rule + ") "
                     else:
-                        new_rule_spec += i + " "
-                rules[rule_num] = new_rule_spec.strip()
+                        new_rule_spec += rule_part + " "
+                rules_copy[rule_num] = new_rule_spec.strip()
 
-    return rules[0].replace(' ', '')
+    return rules_copy[0].replace(' ', '')
 
 def count_valid_messages(rules, messages):
     target_rule_pattern = re.compile("^" + simplify(rules) + "$")    
     valid_messages = []
     for message in messages:
         if target_rule_pattern.match(message):
+            #print("valid message: " + message)
             valid_messages.append(message)
     return len(valid_messages)
 
-def run(label, input_file):
-    rules, messages = read_input(input_file)
+def run(label, input_file1, input_file2):
+    rules, messages = read_input(input_file1)
     print("{} 1: {}".format(label, count_valid_messages(rules, messages)))
 
-run("test", TEST_INPUT_FILE)
-run("part", INPUT_FILE)
+    rules, messages = read_input(input_file2)
+    rules[8] = "42 | 42 8"         # simplifies to (42)+
+    rules[11] = "42 31 | 42 11 31" # simplifies to 42 (42 31)* 31
+    print("{} 2: {}".format(label, count_valid_messages(rules, messages)))
+
+run("test", "day19-input-test", "day19-input-test-2")
+run("part", "day19-input", "day19-input")
